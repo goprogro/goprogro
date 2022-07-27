822135f5210ffc930f52ed7b4319288c050903a4
GoProGro <107775893+goprogro@users.noreply.github.com>
2022-07-27 18:03:58 -0500

    Update single-channel-relay.ino
    updated code

--- sensors/single-channel-relay/single-channel-relay.ino
+++ sensors/single-channel-relay/single-channel-relay.ino
@@ -2,7 +2,7 @@
 
 void setup() {
   Serial.begin(9600);
-   pinMode(RELAY01, OUTPUT);  // Configure pin 0 as OUTPUT
+  pinMode(RELAY01, OUTPUT);  // Configure pin 0 as OUTPUT
 
   digitalWrite(RELAY01,HIGH); // Turn Relay 01 OFF
 }
@@ -10,6 +10,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(RELAY01,LOW);  // Turn Relay 01 ON
-  delay(500);                 // Wait 500 ms
-  digitalWrite(RELAY01,HIGH);  // Turn Relay 01 ON
+  Serial.println( "Relay ON");
+  delay(1000);                 // Wait 1000 ms
+  digitalWrite(RELAY01,HIGH);  // Turn Relay 01 OFF
+  Serial.println( "Relay OFF");
+  delay(1000);                 // Wait 1000 ms
 }
