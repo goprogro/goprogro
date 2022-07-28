4fa7acfe3ac3c8d1daf0ef2debfcb209ab71a5f3
GoProGro <107775893+goprogro@users.noreply.github.com>
2022-07-27 21:01:02 -0500

    Update 8-channel-relay.ino
    added println

--- sensors/8-channel-relay/8-channel-relay.ino
+++ sensors/8-channel-relay/8-channel-relay.ino
@@ -8,58 +8,76 @@
 #define RELAY08 7 //Connect RELAY08 to pin 7
 
 void setup() {
-   pinMode(RELAY01, OUTPUT);  	// Configure pin 0 as OUTPUT
-   pinMode(RELAY02, OUTPUT);  	// Configure pin 1 as OUTPUT
-   pinMode(RELAY03, OUTPUT);  	// Configure pin 2 as OUTPUT
-   pinMode(RELAY04, OUTPUT);  	// Configure pin 3 as OUTPUT
-   pinMode(RELAY05, OUTPUT);  	// Configure pin 4 as OUTPUT
-   pinMode(RELAY05, OUTPUT);  	// Configure pin 5 as OUTPUT
-   pinMode(RELAY06, OUTPUT);  	// Configure pin 6 as OUTPUT
-   pinMode(RELAY07, OUTPUT);  	// Configure pin 7 as OUTPUT
-   pinMode(RELAY08, OUTPUT);  	// Configure pin 8 as OUTPUT
-	
-  digitalWrite(RELAY01,HIGH); 	// Turn Relay 01 OFF
-  digitalWrite(RELAY02,HIGH); 	// Turn Relay 02 OFF
-  digitalWrite(RELAY03,HIGH); 	// Turn Relay 03 OFF
-  digitalWrite(RELAY04,HIGH); 	// Turn Relay 04 OFF
-  digitalWrite(RELAY05,HIGH); 	// Turn Relay 05 OFF
-  digitalWrite(RELAY06,HIGH); 	// Turn Relay 06 OFF
-  digitalWrite(RELAY07,HIGH); 	// Turn Relay 07 OFF
-  digitalWrite(RELAY08,HIGH); 	// Turn Relay 08 OFF
+	Serial.begin(9600);
+	pinMode(RELAY01, OUTPUT);  	// Configure pin 0 as OUTPUT
+	pinMode(RELAY02, OUTPUT);  	// Configure pin 1 as OUTPUT
+	pinMode(RELAY03, OUTPUT);  	// Configure pin 2 as OUTPUT
+	pinMode(RELAY04, OUTPUT);  	// Configure pin 3 as OUTPUT
+	pinMode(RELAY05, OUTPUT);  	// Configure pin 4 as OUTPUT
+	pinMode(RELAY05, OUTPUT);  	// Configure pin 5 as OUTPUT
+	pinMode(RELAY06, OUTPUT);  	// Configure pin 6 as OUTPUT
+	pinMode(RELAY07, OUTPUT);  	// Configure pin 7 as OUTPUT
+	pinMode(RELAY08, OUTPUT);  	// Configure pin 8 as OUTPUT
+
+	digitalWrite(RELAY01,HIGH); 	// Turn Relay 01 OFF
+	digitalWrite(RELAY02,HIGH); 	// Turn Relay 02 OFF
+	digitalWrite(RELAY03,HIGH); 	// Turn Relay 03 OFF
+	digitalWrite(RELAY04,HIGH); 	// Turn Relay 04 OFF
+	digitalWrite(RELAY05,HIGH); 	// Turn Relay 05 OFF
+	digitalWrite(RELAY06,HIGH); 	// Turn Relay 06 OFF
+	digitalWrite(RELAY07,HIGH); 	// Turn Relay 07 OFF
+	digitalWrite(RELAY08,HIGH); 	// Turn Relay 08 OFF
 }
 
 void loop() {
 
-  digitalWrite(RELAY01,LOW);  	// Turn Relay 01 ON
-  delay(500);                 	// Wait 500 ms
-  digitalWrite(RELAY01,HIGH);  	// Turn Relay 01 ON
-  delay(1000);
-  digitalWrite(RELAY02,LOW);  	// Turn Relay 02 ON
-  delay(500);                 	// Wait 500 ms
-  digitalWrite(RELAY02,HIGH);  	// Turn Relay 02 ON
-  delay(1000);
-  digitalWrite(RELAY03,LOW);  	// Turn Relay 03 ON
-  delay(500);                 	// Wait 500 ms
-  digitalWrite(RELAY03,HIGH);  	// Turn Relay 03 ON
-  delay(1000);                 	// Wait 1000 ms
-  digitalWrite(RELAY04,LOW);  	// Turn Relay 04 ON
-  delay(500);                 	// Wait 500 ms
-  digitalWrite(RELAY04,HIGH);  	// Turn Relay 04 ON
-  delay(1000);                 	// Wait 1000 ms
-  digitalWrite(RELAY05,LOW);  	// Turn Relay 05 ON
-  delay(500);                 	// Wait 500 ms
-  digitalWrite(RELAY05,HIGH);  	// Turn Relay 05 ON
-  delay(1000);                 	// Wait 1000 ms
-  digitalWrite(RELAY06,LOW);  	// Turn Relay 06 ON
-  delay(500);                 	// Wait 500 ms
-  digitalWrite(RELAY06,HIGH);  	// Turn Relay 06 ON
-  delay(1000);                 	// Wait 1000 ms
-  digitalWrite(RELAY07,LOW);  	// Turn Relay 07 ON
-  delay(500);                 	// Wait 500 ms
-  digitalWrite(RELAY07,HIGH);  	// Turn Relay 07 ON
-  delay(1000);                 	// Wait 1000 ms
-  digitalWrite(RELAY08,LOW);  	// Turn Relay 08 ON
-  delay(500);                 	// Wait 500 ms
-  digitalWrite(RELAY08,HIGH);  	// Turn Relay 08 ON
-  delay(1000);                 	// Wait 1000 ms
+	digitalWrite(RELAY01,LOW);  	// Turn Relay 01 ON
+	Serial.println( "Relay 1 ON");
+	delay(500);                 	// Wait 500 ms
+	digitalWrite(RELAY01,HIGH);  	// Turn Relay 01 OFF
+	Serial.println( "Relay 1 OFF");
+	delay(1000);
+	digitalWrite(RELAY02,LOW);  	// Turn Relay 02 ON
+	Serial.println( "Relay 2 ON");
+	delay(500);                 	// Wait 500 ms
+	digitalWrite(RELAY02,HIGH);  	// Turn Relay 02 OFF
+	Serial.println( "Relay 2 OFF");
+	delay(1000);
+	digitalWrite(RELAY03,LOW);  	// Turn Relay 03 ON
+	Serial.println( "Relay 3 ON");
+	delay(500);                 	// Wait 500 ms
+	digitalWrite(RELAY03,HIGH);  	// Turn Relay 03 OFF
+	Serial.println( "Relay 3 OFF");
+	delay(1000);                 	// Wait 1000 ms
+	digitalWrite(RELAY04,LOW);  	// Turn Relay 04 ON
+	Serial.println( "Relay 4 ON");
+	delay(500);                 	// Wait 500 ms
+	digitalWrite(RELAY04,HIGH);  	// Turn Relay 04 OFF
+	Serial.println( "Relay 4 OFF");
+	delay(1000);                 	// Wait 1000 ms
+	digitalWrite(RELAY05,LOW);  	// Turn Relay 05 ON
+	Serial.println( "Relay 5 ON");
+	delay(500);                 	// Wait 500 ms
+	digitalWrite(RELAY05,HIGH);  	// Turn Relay 05 OFF
+	Serial.println( "Relay 5 OFF");
+	delay(1000);                 	// Wait 1000 ms
+	digitalWrite(RELAY06,LOW);  	// Turn Relay 06 ON
+	Serial.println( "Relay 6 ON");
+	delay(500);                 	// Wait 500 ms
+	digitalWrite(RELAY06,HIGH);  	// Turn Relay 06 OFF
+	Serial.println( "Relay 6 OFF");
+	delay(1000);                 	// Wait 1000 ms
+	digitalWrite(RELAY07,LOW);  	// Turn Relay 07 ON
+	Serial.println( "Relay 7 ON");
+	delay(500);                 	// Wait 500 ms
+	digitalWrite(RELAY07,HIGH);  	// Turn Relay 07 OFF
+	Serial.println( "Relay 7 OFF");
+	delay(1000);                 	// Wait 1000 ms
+	digitalWrite(RELAY08,LOW);  	// Turn Relay 08 ON
+	Serial.println( "Relay 8 ON");
+	delay(500);                 	// Wait 500 ms
+	digitalWrite(RELAY08,HIGH);  	// Turn Relay 08 OFF
+	Serial.println( "Relay 8 OFF");
+	delay(1000);                 	// Wait 1000 ms
 }
+
